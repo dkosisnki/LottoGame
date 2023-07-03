d5bf3347a03f2d3738626fc5bcdf3467dcc7d770
Damian <d.kosinski13@wp.pl>
2023-07-03 19:25:13 +0200

    added config for integration test and required dependencies

---
+++ src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -0,0 +1,14 @@
+package pl.lotto.feature;
+
+
+import org.junit.jupiter.api.Test;
+import pl.lotto.BaseIntegrationTest;
+
+public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void shouldUserWinAndSystemShouldGenerateWinners(){
+
+    }
+
+}
